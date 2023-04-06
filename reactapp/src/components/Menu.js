2eb9182c48f65e994d2f70065384e0377043daf8
Liubov <lrlr2393@gmail.com>
2023-04-06 12:07:52 +0500

    + Список статей в зависимости от категории, Статья по id, новое название

--- reactapp/src/components/Menu.js
+++ reactapp/src/components/Menu.js
@@ -1,13 +1,19 @@
-const Menu = () => {
+import { Link } from 'react-router-dom'
+
+const CategoryItem = ({category}) => {
+    return (
+<li> <Link to={`/${category.slug}`}>{category.name}</Link></li>
+)}
+
+const Menu = ({categories}) => {
     return (
         <header>
             <div class="container">
-                <a href="/" class="logo">HABR</a>
+                <a href="/" class="logo">Okay Habr</a>
                 <nav>
                     <ul class='nav'>
-                        <li><a href="">Главная</a></li>
-                        <li><a href="">Дизайн</a></li>
-                        <li><a href="">Маркетинг</a></li>
+                        <li><a href="/">Главная</a></li>
+{categories.map((category)=> <CategoryItem category={category}/> )}
                         <li><a href="">Личный кабинет</a></li>
                     </ul>
                 </nav>
