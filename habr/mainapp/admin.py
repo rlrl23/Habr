5146c9b43b5b63af635938838b6c9c2cc9ec9b92
Liubov <lrlr2393@gmail.com>
2023-05-10 18:25:07 +0500

    + author, moderator to admin

--- habr/mainapp/admin.py
+++ habr/mainapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User, Category, Article, Comment, Like
+from .models import User, Category, Article, Comment, Like, Author, Moderator
 
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Article)
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(Author)
+admin.site.register(Moderator)
