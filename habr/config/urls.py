dffd9a782c8b5294e33f740f9b321738a337731a
rlrl23 <rlrl23@mail.ru>
2023-06-05 09:15:14 +0500

    fix problems

--- habr/config/urls.py
+++ habr/config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from mainapp.views import ArticleViewSet, CategoryViewSet, AuthorViewSet, CommentViewSet, LikeViewSet, ModeratorViewSet, ArticlesList, MyToken
+from mainapp.views import ArticleViewSet, CategoryViewSet, AuthorViewSet, CommentViewSet, LikeViewSet, ModeratorViewSet, MyToken
 from rest_framework.authtoken import views
 
 router = DefaultRouter()
